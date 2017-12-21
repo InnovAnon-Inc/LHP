ea38d496a4f6bd96934b7a3e3343cb19135ae4ba
Shiva, Lord of Destruction <laurence.a.maddox@gmail.com>
2017-12-20 22:52:51 -0600

    fixed beggar text

--- testing.js
+++ testing.js
@@ -1,31 +1,48 @@
+
+
 function test() {
 	var con = document.getElementById("cnsl");
-	con.value = drawCircle.toString();
-	drawCircle('test-canvas');
+	con.value = drawCircle.toString() + "\n" +
+		"drawCircle(\"testing-canvas\")";
+	drawCircle("testing-canvas");
 }
 
 function test2() {
 	var con = document.getElementById("cnsl");
-	con.value = "alert (test 2)";
-	drawCircle('test-canvas');
+	con.value = "alert ('test 2')";
+	drawCircle("testing-canvas");
 }
 
-function showCircleCall() { showCall (circle, 'testing-canvas') }
+function showCircleCall() { showCall (circle, "testing-canvas") }
+
+function showCircleCall2() {
+	var con = document.getElementById("cnsl");
+	con.value = drawCircle.toString() + "\n" +
+		"drawCircle(\"testing-canvas\")";
+	drawCircle("testing-canvas");
+}
+
+
+
+
+
+
 
 function showCall(f, a) {
 	var con = document.getElementById("cnsl");
 	con.value = f.toString();
-	f(a);
+	con.value += "\n";
+	con.value += f + "(" + a + ")";
+	//f(a);
 }
 
 function drawCircle(graphics) {
-	var width=100;
-	var height=100;
-
-	var l = Math.min(width, height);
-
 	var c = document.getElementById(graphics);
 	var ctx = c.getContext("2d");
+
+	var width=c.width;
+	var height=c.height;
+	var l = Math.min(width, height);
 
 	ctx.beginPath();
 	ctx.arc(width/2,height/2,l/2,0,2*Math.PI);
