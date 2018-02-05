500e3b3cea5bcb22726ed3e1ee28ae81233edf48
lmaddox <laurence.a.maddox@gmail.com>
2018-02-05 01:32:24 -0600

    changed link color

--- study/music/music1.js
+++ study/music/music1.js
@@ -309,7 +309,7 @@ try{
 			}
 		}
 		
-		this.primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43];
+		this.primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61];
 		this.M  = product (this.primes.slice (0, this.lines.length));
 		this.MP = product (this.primes.slice (0, this.linesP.length));
 		this.mLm = measureLength * measures;
