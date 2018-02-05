a0b8018d1d4827564328d74efb7bea3a3de153c8
lmaddox <laurence.a.maddox@gmail.com>
2018-02-05 01:36:13 -0600

    changed link color

--- study/music/music1.js
+++ study/music/music1.js
@@ -363,13 +363,13 @@ try {
 					//if (k == this.MP) k = 0;
 			}
 			*/
-			for (lll = 0; lll < linesP.length; lll++) {
-				if (m % primes[linesP.length - lll - 1] != 0)
-					linesP[lll].play (now, lpP[lll][p][c]);
-			}
 			for (lll = 0; lll < lines.length; lll++) {
 				if (m % primes[lines.length - lll - 1] == 0)
 					lines[lll].play (now, lp[lll][p][c]);
+			}
+			for (lll = 0; lll < linesP.length; lll++) {
+				if (m % primes[linesP.length - lll - 1] != 0)
+					linesP[lll].play (now, lpP[lll][p][c]);
 			}
 	
 			now += mLm;
