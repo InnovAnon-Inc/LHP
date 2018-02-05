d2ecf7deae69c6c9847aea6533737330ab64c2ee
lmaddox <laurence.a.maddox@gmail.com>
2018-02-05 01:23:53 -0600

    changed link color

--- study/music/music1.js
+++ study/music/music1.js
@@ -229,7 +229,7 @@ Line.prototype.play = function (time, chord) {
 
 function Song (context) {
 try{
-		var measureLength = 60 / 40;
+		var measureLength = 2 * 60 / 40;
 		var measures = 1;
 		var bf = 432;
 		this.scale = [1/1, 16/15, 9/8, 6/5, 5/4, 4/3, 7/5, 3/2,	8/5, 5/3, 16/9, 15/8];
