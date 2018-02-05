8fd7537a697f5ae4429dc8a28bd85d7d5ee08a60
lmaddox <laurence.a.maddox@gmail.com>
2018-02-05 04:17:23 -0600

    changed link color

--- study/music/music1.js
+++ study/music/music1.js
@@ -438,7 +438,7 @@ try {
 				if (p == scale.length) p = 0;
 			}
 			
-			setTimeout(cycle2, Math.max (0, (now - context.currentTime) * 1000 - 1000));
+			setTimeout(cycle2, Math.max (0, (now - context.currentTime) * 1000 - 100));
 		}
 		function cycle () {
 		
