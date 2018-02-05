0e3bae4bafd0e925e8746d4bb1a108f3b448f546
lmaddox <laurence.a.maddox@gmail.com>
2018-02-05 03:32:58 -0600

    changed link color

--- study/music/music1.js
+++ study/music/music1.js
@@ -284,7 +284,7 @@ try{
 		//var pulses  = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2];
 		//var pulsesP = [4, 3, 5, 2, 4, 3, 5, 2, 4, 3, 5, 2];
 		var pulses  = [1, 2, 3, 4, 5, 4, 3, 2, 1];
-		var pulsesP = [5, 4, 3, 4];
+		var pulsesP = [5, 4, 3, 2, 3, 4];
 		this.lines  = new Array (pulses.length);
 		this.linesP = new Array (pulsesP.length);
 		var pl;
