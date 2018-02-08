7cea1f2f90c749f1d75a39e427ae1d64333a54ce
lmaddox <laurence.a.maddox@gmail.com>
2018-02-08 15:13:06 -0600

    handbook

--- study/music/lib/piano2.js
+++ study/music/lib/piano2.js
@@ -2,14 +2,16 @@
 
 function applyScale (baseFreq, scale, octaves, octave) {
 	var ret = new Array (scale.length * octaves);
-	var i;
-	for (i = 0; i < ret.length; i++)
-		ret[i] = baseFreq * Math.pow (2, octave + i) * scale[i % scale.length];
+	var i, j, k;
+	for (i = k = 0; i < octaves; i++)
+		for (j = 0; j < scale.length; j++, k++)
+			ret[k] = baseFreq * Math.pow (2, octave + i) * scale[j];
 	return ret;
 }
 
 function Piano2 (context, bf, scale, octaves, octave, gain, richness) {
 	var notes = applyScale (bf, scale, octaves, octave);
+	alert (notes);
 	this.piano = new Piano (context, notes, gain, richness);
 }
 Piano2.prototype.trigger = function(time, freqs, duration) {
